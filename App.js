3ca2280334ac0b89047471b9b8325a2aaf0f32e5
LucaPalminteri <lucapalminteri02@gmail.com>
2022-05-09 18:19:02 -0300

    Replaced app for some new code, keep practicing

--- App.js
+++ App.js
@@ -1,92 +1,143 @@
 import {useState, useEffect} from 'react'
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView, Image, Button } from "react-native";
+import { StyleSheet, ScrollView, Text, View, SafeAreaView, Image, Button, TextInput  } from "react-native";
 
 
 export default function App() {
 
-  const [count,setCount] = useState(0)
-  const [min,setMin] = useState(0)
+  const [number,setNumber] = useState()
 
   useEffect(()=>{
-    setTimeout(()=>{
-      setCount(p=>p+1)
-      if(count >= 59) {
-        setMin(p=>p+1)
-        setCount(0)
-      }
-    },1000)
   },[])
-  
+
+  function onChangeNumber(e) {
+    setNumber(e)
+  }
+
+  function showNumber() {
+    alert(number)
+  }
 
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
       <StatusBar style="auto" />
-      
-      <Text style={styles.text}>Counter</Text>
-      <Text style={styles.text}>{min}m :{count}s</Text>
-      <View style={styles.buttons}>
-        <Button 
-          onPress={()=>{setCount(x=>x-1)}}
-          title='-1'
-        />
-        <Button 
-          onPress={()=>{setCount(0)}}
-          title='Reset'
-        />
-        <Button 
-          onPress={()=>{setCount(x=>x+1)}}
-          title='+1'
-          style={styles.button}
-        />
-      </View>
-      <Text></Text>
-      <View style={styles.buttons}>
-        <Button 
-          onPress={()=>{setCount(x=>x-10)}}
-          title='-10'
-        />
-        <Button 
-          onPress={()=>{setCount(x=>x+10)}}
-          title='+10'
-          style={styles.button}
-        />
-      </View>
+      <Text style={styles.title}>Luca Palminteri</Text>
+      <Text style={styles.text}>
+        {'\n'}
+        This a paragraph about how much I love coding Apps, and React Native gave me the ability to transform my web
+        development knowledge into app development. This is something I'm really excited about.
+        {'\n'}
+      </Text>
+      <Image 
+        style={styles.image}
+        source={{
+          uri: 'https://lucapalminteri.com/static/media/luca-five.09fc7dd50b27e48fb99e.png'
+        }}
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeNumber}
+        value={number}
+        placeholder="useless placeholder"
+        keyboardType="default"
+      />
+      <Text style={styles.text}>
+        {'\n'}
+        This a paragraph about how much I love coding Apps, and React Native gave me the ability to transform my web
+        development knowledge into app development. This is something I'm really excited about.
+        {'\n'}
+      </Text>
+      <Text style={styles.text}>
+        {'\n'}
+        This a paragraph about how much I love coding Apps, and React Native gave me the ability to transform my web
+        development knowledge into app development. This is something I'm really excited about.
+        {'\n'}
+      </Text>
+      <Text style={styles.text}>
+        {'\n'}
+        This a paragraph about how much I love coding Apps, and React Native gave me the ability to transform my web
+        development knowledge into app development. This is something I'm really excited about.
+        {'\n'}
+      </Text>
+      <Text style={styles.text}>
+        {'\n'}
+        This a paragraph about how much I love coding Apps, and React Native gave me the ability to transform my web
+        development knowledge into app development. This is something I'm really excited about.
+        {'\n'}
+      </Text>
+
 
       <View style={styles.footer}>
-        <Text>Footer</Text>
+        <Button
+          title="Show Input"
+          onPress={showNumber}
+          style={styles.button}
+          color='#565656'
+        />
+        <Button 
+          title="+1"
+          onPress={() => undefined}
+          style={styles.button}
+          color='#565656'
+        />
       </View>
+      <View style={styles.space}></View>
       
-    </View>
+        
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     width: '100%',
-    height: 500,
+    height: '100%',
+    display: 'flex',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    backgroundColor: '#dcdcdc',
+    position: 'relative'
+  },
+  title: {
+    paddingTop: 40,
+    fontSize: 40,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    backgroundColor: '#ababab',
+    width: '100%',
+  },
+  text: {
+    paddingHorizontal: 10,
+  },
+  image: {
+    width: 300,
+    height: 300
+  },
+  footer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    backgroundColor: '#898989',
+    width: '100%',
+    height: 75,
+    paddingHorizontal: 0,
+    position: 'absolute',
+    bottom: 0,
+  },
+  button: {
     display: 'flex',
     justifyContent: 'center',
     alignContent: 'center',
-    backgroundColor: '#fff',
   },
-  text: {
-    backgroundColor: '#FFF',
-    textAlign: 'center',
-    padding: 40,
-    fontSize: 60
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
-  buttons: {
-    flexDirection: 'row',
-    justifyContent: 'space-around'
-  },
-  button: {
-    fontSize: 30,
-    backgroundColor: 'red'
-  },
-  footer: {
-    backgroundColor: 'blue', 
-    position: 'relative',
-    top: 0
+  space: {
+    height: 75
   }
 });
